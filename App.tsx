9a132a7e790dbc83ed9d9edb805e38f7065e4ef3
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2024-03-25 16:25:40 +0900

    Updates

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import "./App.scss";
+
+export const App = () => {
+  const name: string = "山崎　和喜";
+  const profile: string = "持っている資格：基本情報技術者";
+  return (
+    <div className="App">
+      <h1>kazuki's Portfolio</h1>
+      <Card sx={{ width: 500, height: 500 }} className="card">
+        <CardContent>
+          <Avatar
+            alt="sample"
+            src=""
+            sx={{ width: 200, height: 200 }}
+            className="img"
+          />
+          <Typography variant="h4" className="name">
+            {name}
+          </Typography>
+          <Typography variant="h5" className="profile">
+            {profile}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
---
+++ index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { hydrateRoot } from "react-dom/client";
+
+const container = document.getElementById("root") as HTMLElement;
+hydrateRoot(container, <App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
